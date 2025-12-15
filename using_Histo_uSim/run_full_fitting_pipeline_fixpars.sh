225d0be6017108125fbf49f975b360a6c2b32877
fgrussuvhio <90615695+fgrussuvhio@users.noreply.github.com>
2025-12-15 10:58:32 +0100

    Update run_full_fitting_pipeline_fixpars.sh

--- using_Histo_uSim/run_full_fitting_pipeline_fixpars.sh
+++ using_Histo_uSim/run_full_fitting_pipeline_fixpars.sh
@@ -1,1 +1,22 @@
 ### TO BE WRITTEN
+
+
+# As an example, create select a small sub-protocol of diffusion MRI measurements at fixed diffusion time
+fslroi XX
+echo "jksadjaksd" > scheme_fixed_diffusion time
+
+# Create dictionary with fixed kappa, for example kappa = 25 um/s
+
+
+# Run fitting fixing D0in to 2.4 um2/ms
+# Remember that the tissue parameters are defined in the following ranges
+# - fin: 
+# - mCS: 
+# - varCS: 
+# - skewCS:
+# - vCScyl:
+# - vCSsph:
+# - D0in: 0.8 to 3 um2/ms
+# - D0ex: 0.8 to 3 um2/ms
+# - kappa: 0 to 40 um/s
+
